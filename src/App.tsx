bc7e1d780a64622436604c03f1d3d01ab2e072e6
itzkunal01 <kunal.sharmag12@gmail.com>
2023-12-14 12:38:39 +0530

    updating code of typescript practice with personal interface.

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,11 @@ function App() {
   return (
     <div className="text-center bg-white min-h-screen">
       {/* Onclick optional for 1st button but text is compulsory. */}
-      <MyButton text="Click Me" />
+      {/* <MyButton text="Click Me" /> */}
       <MyButton onClick={() => alert("Okay")} text="Click Me Again" />
       {/* MYButton can be number or boolean also. */}
-      <MyButton text={1} />
-      <MyButton text={true} />
+      {/* <MyButton text={1} /> */}
+      {/* <MyButton text={true} /> */}
       <TodoList />
     </div>
   );
--- src/components/MyButton.tsx
+++ src/components/MyButton.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface MyButtonProps {
   text: string | number | boolean; // Here " | " is OR operater for string , number and boolean.
@@ -6,16 +6,42 @@ interface MyButtonProps {
   something?: boolean;
 }
 
+interface Book {
+  name: string;
+  price: number;
+}
+
 // type props = { text: string };
 // 1st approach
 // const MyButton = (props: { text: string }) => {
 // 2nd approach
 const MyButton: React.FC<MyButtonProps> = (props) => {
+  // const [value, setValue] = useState(1); // implicit casting
+  // OR always give type to useState
+  // const [value, setValue] = useState<string>("b"); // explicit casting , always prefer this casting
+  const [value, setValue] = useState<Book>({
+    name: "One",
+    price: 10,
+  });
   return (
     <>
+      {/* <h3>{value}</h3> */}
+      <h3>
+        Name: {value.name} (Rs. {value.price})
+      </h3>
       <button
         className="bg-[#c9c6c6] p-2 rounded-lg m-5"
-        onClick={props.onClick}
+        // onClick={props.onClick}
+        // onClick={() => setValue(value + 1)}
+        onClick={() => {
+          // 1.
+          // setValue("a");
+          // console.log("value changed from b to a");
+
+          // 2.
+          // For personal interface (production-level-app)
+          setValue({ name: "Two", price: 20 + 1 });
+        }}
       >
         {props.text}
       </button>
